f5337152d69b52ee0a873b48b93817a31b8c7a20
Joylish <yeonzzu45@ajou.ac.kr>
2019-07-18 17:31:18 +0900

    make about, header js file and use in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React,{Component} from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import Home from './components/Home'
+import About from './components/About'
 
 class App extends Component{
   
   // lifecycle
   // constructor(){}
+
   componentDidMount(){
     console.log('Component DID MOUNT')
   }
@@ -18,13 +21,9 @@ class App extends Component{
     return (
       <BrowserRouter>
       <div className="App">
-      <Header name ={name} foo="Hi!" />
-      <Route exact path ="/" Component {<div> Home </div>} />
-      
-      <Route path ={match.path} render{ () => {
-        <div>List</div>
-      }}/>
-    
+        <Route exact path ="/" component = {Home} />
+        <Route exact path = "/about" component = {About} />
+        <Header name = {name } foo = "hi"/>
       </div>
       </BrowserRouter>
       //browserrouter라는 컴포넌트
