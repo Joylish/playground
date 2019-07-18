f567320b2eeb1cd07adadc12ae220d49ef64e5fb
Joylish <yeonzzu45@ajou.ac.kr>
2019-07-18 17:15:11 +0900

    make error in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
@@ -15,9 +16,18 @@ class App extends Component{
     const name = "Joylish";
 
     return (
+      <BrowserRouter>
       <div className="App">
       <Header name ={name} foo="Hi!" />
+      <Route exact path ="/" Component {<div> Home </div>} />
+      
+      <Route path ={match.path} render{ () => {
+        <div>List</div>
+      }}/>
+    
       </div>
+      </BrowserRouter>
+      //browserrouter라는 컴포넌트
     );
   }
 }
