b0214bf4830cf7f55bb117005e40cf79a23bf1b3
Joylish <yeonzzu45@ajou.ac.kr>
2019-07-23 18:54:20 +0900

    add custom component

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component{
+ class CounterButton extends React.Component{
   state = {
     counter: 0,
   };
@@ -18,6 +18,16 @@ export default class App extends React.Component{
      <Text>{this.state.counter}</Text>
      <Button title ={'click me!'} onPress={this.clickHandler}/>
     </View>
+    );
+  }
+}
+
+export default class App extends React.Component{
+  render(){
+    return (
+      <View style ={styles.container}>
+        <CounterButton />
+      </View>
     );
   }
 }
