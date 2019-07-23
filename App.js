4ea142ce6c9088346197ba7e7c3013d0b7a24bee
Joylish <yeonzzu45@ajou.ac.kr>
2019-07-24 02:47:38 +0900

    add property to component 'CounterButton' and use props which is difficult to understand..

--- App.js
+++ App.js
@@ -1,34 +1,40 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
- class CounterButton extends React.Component{
-  state = {
-    counter: 0,
-  };
+class CounterButton extends React.Component {
 
-  clickHandler = () =>{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: props.counter,
+    };
+  }
+
+  clickHandler = () => {
     this.setState({
       counter: this.state.counter + 1,
     });
   };
 
-  render(){
-  return (
-    <View style={styles.container}>
-     <Text>{this.state.counter}</Text>
-     <Button title ={'click me!'} onPress={this.clickHandler}/>
-    </View>
+  render() {
+    return (
+      <View>
+        <Text>{this.state.counter}</Text>
+        <Button title={'click me!'} onPress={this.clickHandler}/>
+      </View>
     );
   }
 }
 
-export default class App extends React.Component{
-  render(){
+export default class App extends React.Component {
+  render() {
     return (
-      <View style ={styles.container}>
-        <CounterButton />
-        <CounterButton />
-        <CounterButton />
+      <View style={styles.container}>
+        <CounterButton counter={0} />
+        <CounterButton counter={1} />
+        <CounterButton counter={2} />
+        <CounterButton counter={3} />
       </View>
     );
   }
