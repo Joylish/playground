8d02ae0f80aad9646946edda5a084ec56eb76043
Joylish <yeonzzu2@gmail.com>
2020-07-11 23:50:03 +0900

    create 3 Domain related to 3 functions

---
+++ src/com/soft/Domain/CategorySet.java
@@ -0,0 +1,18 @@
+package com.soft.Domain;
+
+public class CategorySet {
+    public String small;
+    public String medium;
+    public String large;
+
+    public CategorySet(String large, String medium, String small){
+        this.large = large;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public CategorySet(String large, String medium){
+        this.large = large;
+        this.medium = medium;
+    }
+}
---
+++ src/com/soft/Domain/Product.java
@@ -0,0 +1,4 @@
+package com.soft.Domain;
+
+public class Product {
+}
