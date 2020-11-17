43d40e588b82111d065aa6b805da624a613e5b38
LeeYeonJu <yeonzzu2@gmail.com>
2020-11-17 14:33:40 +0900

    ADD : WishList 을 localstorage에 저장

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import { onSnapshot } from 'mobx-state-tree';
 
+import App from "./components/App";
 import {WishList} from './models/WishList'
 
-const wishList = WishList.create({
+let initialState = {
   items: [
     {
       name: "Happy Box",
@@ -19,7 +20,22 @@ const wishList = WishList.create({
         "https://st2.depositphotos.com/1000336/8268/i/950/depositphotos_82683780-stock-photo-white-christmas-cake-with-decorations.jpg",
     },
   ],
+}
+
+if(localStorage.getItem("wishListApp")){
+  const json = JSON.parse(localStorage.getItem("wishListApp"));
+  if(WishList.is(JSON)){
+    initialState = json
+  }
+}
+const wishList = WishList.create(initialState);
+
+// 새 스냅샷이 발생하면 onSnapshot 함수에서 
+// 현재 state를 가져와서 로컬 스토리지에 저장
+onSnapshot(wishList, snapshot => {
+  localStorage.setItem("wishListApp", JSON.stringify(snapshot));
 });
+
 ReactDOM.render(<App wishList={wishList}/>, document.getElementById("root"));
 
 // setInterval(()=>{
